221b14e6975e0f64cb7b51b7440f63006f4033ad
wendy <5703@holbertonstudents.com>
2023-01-17 04:13:42 -0800

    fixed E128 continuation line under-indented for visual indent

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 if number > 5:
     print("Last digit of {} is {} and is greater than 5"
-        .format(number, number % 10))
+          .format(number, number % 10))
 elif number == 0:
     print("Last digit of {} is {} and is 0".format(number, number % 10))
 elif number < 6 and number > 0:
     print("Last digit of {} is {} and is less than 6 and not 0"
-        .format(number, number.__str__()[-1]))
+          .format(number, number.__str__()[-1]))
 elif number < 0:
     print("Last digit of {} is -{} and is less than 6 and not 0"
-        .format(number, number.__str__()[-1]))
+          .format(number, number.__str__()[-1]))
