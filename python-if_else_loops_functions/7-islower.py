02973c5d474f3c62f3d267ca37ca29c199f43cbb
wendy <5703@holbertonstudents.com>
2023-01-18 03:34:31 -0800

    check for lowercase character

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in c:
-        if i.islower():
-            return True
-        elif c == '':
-            return False
-        else:
-            return False
+    if c.islower():
+        return True
+    elif c == '':
+        return False
+    else:
+        return False
