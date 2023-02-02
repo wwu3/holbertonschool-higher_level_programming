469ebb202c09b40e4da97a53216186129773c3f3
wendy <5703@holbertonstudents.com>
2023-02-02 04:24:05 -0800

    integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """
-What
-is
-words
+This is the "example" module.
+
+The  module supplies one function, add_integer().  Eg,
+
+>>>add_integer(2, 98)
+100
 """
 
 
 def add_integer(a, b=98):
     """
-    What thet
+    Returns a + b.
     """
-    if type(a) != int and type(a) != float:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
-        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
 
     return int(a+b)
