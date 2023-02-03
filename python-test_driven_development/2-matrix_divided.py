bd91d7b04f5c629b36881faf16907e4ce691d17e
wendy <5703@holbertonstudents.com>
2023-02-02 21:21:26 -0800

    divide a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+This is the "matrix_divided" module.
+
+The module supplies one function, matrix_divided(). Eg,
+
+>>>matrix_divided(12, 3)
+4
+"""
+
+
 def matrix_divided(matrix, div):
-    new_matrix = [] 
+    """
+    Returns new_matrix
+    """
+    new_matrix = []
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
@@ -9,7 +22,8 @@ def matrix_divided(matrix, div):
         new_row = []
         for s in row:
             if type(s) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must
+                                be a matrix(list of lists) of integers/floats")
             new_row.append(int(s / div))
         new_matrix.append(new_row)
     return new_matrix
