1fe87acffea19ebfa06cc4f8c9bf505f1fc7e9b4
wendy <5703@holbertonstudents.com>
2023-02-03 18:17:26 -0800

    string representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -44,7 +44,7 @@ class Rectangle:
 
     def perimeter(self):
         if self.height == 0 or self.width == 0:
-            print()
+            print(str())
             return 0
         return 2 * (self.height + self.width)
 
