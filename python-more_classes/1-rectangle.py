cac1b7a4407e6b32dd20a709d036b1ec738b69d4
wendy <5703@holbertonstudents.com>
2023-02-03 17:15:14 -0800

    defines a rectangle, property and setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
 
     """Initializes the data"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
     
     @property
     def height(self):
