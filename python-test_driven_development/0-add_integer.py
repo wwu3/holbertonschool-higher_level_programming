ca66f54dcb5d46002ec9eec74084c23431a9da93
wendy <5703@holbertonstudents.com>
2023-02-04 15:45:34 -0800

    adds doctest

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,9 +15,9 @@ def add_integer(a, b=98):
     Returns a + b.
     """
     if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
+        raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
+        raise TypeError('b must be an integer')
 
     return int(a+b)
 
