dfea9be762275b01a50a876b3d2c0b527426c1eb
wendy <5703@holbertonstudents.com>
2023-02-03 18:20:58 -0800

    string representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -48,6 +48,7 @@ class Rectangle:
         return 2 * (self.height + self.width)
 
     def __str__(self):
+        return "" if self.height == 0 or self.width == 0
         st = "#"
         st = st * self.width + "\n"
         st = self.height * st
