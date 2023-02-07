0d6b14020454d7649ab10edda17738f643a0fbee
wendy <5703@holbertonstudents.com>
2023-02-07 00:01:09 -0800

    public instance method, integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,9 +9,9 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if not type(value) is int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("name must be an integer")
         else:
             if value < 0:
-                raise ValueError("<distance> must be greater than 0")
+                raise ValueError("distance must be greater than 0")
             elif value == 0:
-                raise ValueError("<age> must be greater than 0")
+                raise ValueError("age must be greater than 0")
