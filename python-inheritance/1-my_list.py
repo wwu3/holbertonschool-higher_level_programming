5581485725abdd60f67519ee99b808f9e29e6208
wendy <5703@holbertonstudents.com>
2023-02-07 04:03:58 -0800

    sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+"""Represents MyList"""
+
+
+class MyList(list):
+    """A class defined as MyList that inherits from super class called list"""
+    def print_sorted(self):
+        print(sorted(self))
