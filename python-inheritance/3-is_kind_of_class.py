5489e2ee65fde281a808b97e559b2417c41d3c65
wendy <5703@holbertonstudents.com>
2023-02-07 23:44:27 -0800

    isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Represents same class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Defines a function called is_kind_of_class pass two arguments
+    """
+    return isinstance(obj, a_class)
