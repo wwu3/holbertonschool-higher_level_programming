c50cbc221309b258a997e399b9eed3426b37e3eb
wendy <5703@holbertonstudents.com>
2023-02-08 00:01:16 -0800

    subclass

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Represents subclass"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Defines a functiion called inherits_from
+    """
+    return isinstance(type(obj), a_class)
