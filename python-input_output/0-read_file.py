ed9b8d44e9f9b0c5f607c6491d71d8f11642c67d
wendy <5703@holbertonstudents.com>
2023-02-08 03:19:58 -0800

    adds README

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Read file"""
+
+
 def read_file(filename=""):
+    """
+    Define a function called read_file, passed the argument filename
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
