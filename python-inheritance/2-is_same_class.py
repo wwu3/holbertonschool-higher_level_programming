aa89b91ae3b118653b0df1991a7690cf9bf1dda9
wendy <5703@holbertonstudents.com>
2023-02-07 23:14:47 -0800

    exact same object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,4 +4,7 @@
 
 """Define a function called is_same_class"""
 def is_same_class(obj, a_class):
+    """
+    Defines a function called is_same_class
+    """
     return isinstance(type(obj), type(a_class))
