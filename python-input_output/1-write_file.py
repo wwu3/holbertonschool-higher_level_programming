09ac74c700ef827f662dbe56dc19983b54935f86
wendy <5703@holbertonstudents.com>
2023-02-09 15:34:31 -0800

    write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Write a file"""
+
+
 def write_file(filename="", text=""):
+    """
+    define a function write_file with arguments filename, text
+    """    
     with open(filename, 'w', encoding="utf-8") as f:
         s = str(text)
         f.write(s)
