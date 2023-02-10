a7ea04622b9da6c4f2a14c38e7a20252657088cc
wendy <5703@holbertonstudents.com>
2023-02-09 18:16:05 -0800

    Json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""To JSON string"""
+
+
+def to_json_string(my_obj):
+    """
+    define a function to_json_string pass the argument my_obj
+    """
+    return my_obj
