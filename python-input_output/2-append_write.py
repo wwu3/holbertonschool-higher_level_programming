34148d5eb8f685686c8ccf542a65ca2dcfbd3828
wendy <5703@holbertonstudents.com>
2023-02-09 18:01:27 -0800

    append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Append to a file"""
+
+
+def append_write(filename="", text=""):
+    """define a function append_write"""    
+    count = 
+    with open(filename, 'a+', encoding="utf-8") as f:
+        count = f.write(text)
+    return count
