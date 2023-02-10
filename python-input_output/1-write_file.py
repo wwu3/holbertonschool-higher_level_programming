bb3b8c318c6297eacc6014f82ef23fd8041c8ddd
wendy <5703@holbertonstudents.com>
2023-02-09 17:30:22 -0800

    write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,8 +5,8 @@
 def write_file(filename="", text=""):
     """
     define a function write_file with arguments filename, text
-    """ 
-    count = 0   
+    """
+    count = 0
     with open(filename, 'w', encoding="utf-8") as f:
         count = f.write(text)
     return count
