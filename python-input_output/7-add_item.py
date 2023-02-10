3772c47d36b6fccbee45b5bffc97bbda42811ac2
wendy <5703@holbertonstudents.com>
2023-02-10 15:07:23 -0800

    load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+
+
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+import two functions
+"""
+args = load_from_json_file('add_item.json')
+new_file = save_to_json_file(args + sys.argv[1:], 'add_item.json')
