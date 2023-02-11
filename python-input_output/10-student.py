be2a716c5ee3adeb334cd37a217be1b0b0f543f9
wendy <5703@holbertonstudents.com>
2023-02-10 19:05:41 -0800

    filter python

---
+++ python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a class define as Student"""
+
+
+class Student:
+    """Initializes the datat"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return dict(filter(lambda i: i[0] in attrs, self.__dict__.items()))
