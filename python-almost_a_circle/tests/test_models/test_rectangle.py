2f87822e97a1dfc29c159239397798209fce52bb
wendy <5703@holbertonstudents.com>
2023-02-15 04:13:56 -0800

    test rectangel(1, 2)

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,6 +8,10 @@ class TestRectangle(unittest.TestCase):
     def test_idf(self):
         r = Rectangle(1, 2)
         self.assertEqual(r.id, Base.get_nb_objects())
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
 
 if __name__ == "__main__":
     unittest.main()
