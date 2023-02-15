caef9677c3a39f7b9059f6b79e3b3093b7652a3d
wendy <5703@holbertonstudents.com>
2023-02-15 03:42:22 -0800

    validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,12 +6,12 @@ from models.base import Base
 class Rectangle(Base):
     """Initialize the data"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-    
+
     @property
     def width(self):
         return self.__width
@@ -28,18 +28,34 @@ class Rectangle(Base):
     def y(self):
         return self.__y
 
+    def validate_positive(self, value, name):
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+
+    def validate_positive_or_zero(self, value, name):
+        if not type(value) is int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
     @width.setter
     def width(self, value):
+        self.validate_positive(value, "width")
         self.__width = value
 
     @height.setter
     def height(self, value):
+        self.validate_positive(value, "height")
         self.__height = value
 
     @x.setter
     def x(self, value):
+        self.validate_positive_or_zero(value, "x")
         self.__x = value
 
     @y.setter
     def y(self, value):
+        self.validate_positive_or_zero(value, "y")
         self.__y = value
