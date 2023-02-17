6d305bc4887b5c8996042eb8710f4f1298004c2b
wendy <5703@holbertonstudents.com>
2023-02-17 15:17:04 -0800

    __str__ returns

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,4 +80,4 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        return f"[Rectangle] {self.id} {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
