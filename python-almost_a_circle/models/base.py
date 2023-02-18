715dddb6e82333d655c23246bc94e2574a4a055d
wendy <5703@holbertonstudents.com>
2023-02-17 21:52:29 -0800

    dict to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,4 +24,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        if list_dictionaries is None or []:
+            return "[]"
         return json.dumps(list_dictionaries)
