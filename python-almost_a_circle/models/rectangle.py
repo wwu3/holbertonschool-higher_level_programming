e9de36c8b99e7e34a163982ee1fd54852fb47ecc
wendy <5703@holbertonstudents.com>
2023-02-17 20:06:12 -0800

    *args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -94,4 +94,13 @@ class Rectangle(Base):
         """
         Function update
         """
-        return ("[Rectangle] ({}) {}/{} - {}/{}", args)
+        if args[0] is not None:
+            self.id = args[0]
+        if len(args) > 1 and args[1] is not None:
+            self.width = args[1]
+        if len(args) > 2 and args[2] is not None:
+            self.height = args[2]
+        if len(args) > 3 and args[3] is not None:
+            self.x = args[3]
+        if len(args) > 4 and args[4] is not None:
+            self.y = args[4]
