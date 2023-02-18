4cd03d66d1a6dc51f3c4c55cb6ea0d7898f901eb
wendy <5703@holbertonstudents.com>
2023-02-18 14:13:18 -0800

    updated tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,3 +7,7 @@ class TestBase(unittest.TestCase):
     def test_no_parameters(self):
         b = Base()
         self.assertEqual(b.id, 1)
+
+    def test_json(self):
+        b = Base(6)
+        self.assertEquals(b.to_json_string(), "{id: gfgidf, age: 66, name: Zoe")
