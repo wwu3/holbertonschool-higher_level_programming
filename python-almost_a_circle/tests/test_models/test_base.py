758fc391051c16bfdfc68379fea448972969ecca
wendy <5703@holbertonstudents.com>
2023-02-18 17:15:08 -0800

    Add tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -11,4 +11,4 @@ class TestBase(unittest.TestCase):
     def test_json(self):
         self.assertEquals(Base.to_json_string([{"id": 3}]), '[{"id": 3}]')
         self.assertNotEqual(Base.from_json_string('[{ "id": 89 }]'), '[{ "id": 89 }]')
-        self.assertEqual(Base.from_json_string([{"id": 1}]), '[{"id": 15}]')
+        self.assertEqual(Base.from_json_string('[{"id": 1}]'), [{"id": 1}])
