2c183c1a9a389d7e75b482dcef04f550c150ef92
wendy <5703@holbertonstudents.com>
2023-02-18 17:27:33 -0800

    Add tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,6 +13,8 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.x, 0)
         self.assertEqual(r.y, 0)
         self.assertEqual(r.area(), 2)
+        self.assertEqual(2, r.height)
+        self.assertNotEqual('2', r.height)
 
 if __name__ == "__main__":
     unittest.main()
