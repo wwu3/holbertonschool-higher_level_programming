c4f2e4d570cfa41ac67ba0811421ce746c8b0c62
wendy <5703@holbertonstudents.com>
2023-02-18 17:19:07 -0800

    Add tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,6 +7,10 @@ class TestBase(unittest.TestCase):
     def test_no_parameters(self):
         b = Base()
         self.assertEqual(b.id, 1)
+    
+    def test_parameters(self):
+        b = Base(41)
+        self.assertEqual(b.id, 41)
 
     def test_json(self):
         self.assertEquals(Base.to_json_string([{"id": 3}]), '[{"id": 3}]')
