d011d76034dbb471360d7c0960dc3162eac9c8ae
wendy <5703@holbertonstudents.com>
2023-03-15 03:43:38 -0700

    where states.name = California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- ists all the cities of California that can be found in the database
+
+SELECT cities.name
+FROM cities, states
+WHERE cities state.id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
