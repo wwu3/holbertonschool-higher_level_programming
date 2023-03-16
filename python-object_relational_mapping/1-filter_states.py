f87767af470a7a0f4b09ce8ab4a178ab0a814031
wendy <5703@holbertonstudents.com>
2023-03-16 04:55:53 -0700

    list states start name with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,15 +6,13 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     databasename = sys.argv[3]
-    
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-            passwd=mysql_password, db=databasename)
+                         passwd=mysql_password, db=databasename)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^N'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
