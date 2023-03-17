50b826c4799b1e1270b849a83b79bf863c2a8756
wendy <5703@holbertonstudents.com>
2023-03-16 17:26:27 -0700

    name wehre  matches with state_name_search

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Import MySQLdb
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
+    c = db.cursor()
+    query = ("SELECT * FROM states where name='{}' ORDER BY id ASC"
+             .format(state_name_searched))
+    c.execute(query)
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
