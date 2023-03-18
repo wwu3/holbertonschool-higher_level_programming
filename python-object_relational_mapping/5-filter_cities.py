a39adde6d296132f0176bf008e737f082eb319ec
wendy <5703@holbertonstudents.com>
2023-03-18 00:34:53 -0700

    states.name = %s

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,13 +5,15 @@ Takes in the name of a state as an argument
 import MySQLdb
 import sys
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-state_name = sys.argv[4]
+if __name__ == "__main__":
 
-db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                     passwd=mysql_password, db=database_name)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
 
 cur = db.cursor()
 cities_query = """
