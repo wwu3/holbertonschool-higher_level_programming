531b4d16bcab15aa7882189d7f17992f81d3400c
wendy <5703@holbertonstudents.com>
2023-03-18 03:41:42 -0700

    lists all cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- ists all the cities of California that can be found in the database
 
-SELECT cities.id cities.name
+SELECT cities.id, cities.name
 FROM cities, states
 WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC
