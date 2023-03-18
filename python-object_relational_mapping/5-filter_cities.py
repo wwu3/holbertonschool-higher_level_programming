ad7ed66229fbb07078ed57f5c692d6d0ac598b4d
wendy <5703@holbertonstudents.com>
2023-03-18 01:39:34 -0700

    all cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -15,17 +15,17 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
                          passwd=mysql_password, db=database_name)
 
-cur = db.cursor()
-cities_query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-cur.execute(cities_query, [state_name])
-rows = cur.fetchall()
-for row in rows:
-    print(row[0], end=' ')
-cur.close()
-db.close()
+    cur = db.cursor()
+    cities_query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """
+    cur.execute(cities_query, [state_name])
+    rows = cur.fetchall()
+    for row in rows:
+        print(row[0], end=' ')
+    cur.close()
+    db.close()
