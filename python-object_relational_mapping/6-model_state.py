e724c861191ae84568d56db418c81c4d23588fb4
wendy <5703@holbertonstudents.com>
2023-03-18 19:40:10 -0700

    first state model

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+model_state contains the State class and Base = declarative_base()
+"""
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    db_uri = ("mysql+mysqldb://root@localhost:3306/hbtn_0e_6_usa")
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
