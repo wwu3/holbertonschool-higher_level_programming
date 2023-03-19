afd8a094c1bdcc4b966d711211a3acbfcbe431a9
wendy <5703@holbertonstudents.com>
2023-03-18 18:01:53 -0700

    all cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,12 +6,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql_username> <mysql_password>"
-              "  <database_name>".format(sys.argv[0]))
-        exit(1)
-
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
@@ -21,13 +15,9 @@ if __name__ == "__main__":
                          passwd=mysql_password, db=database_name)
 
     cur = db.cursor()
-    cities_query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """
+    cities_query = ("SELECT cities.name FROM cities JOIN states"
+                    " ON cities.state_id = states.id WHERE states.name = %s"
+                    " ORDER BY cities.id ASC")
     cur.execute(cities_query, [state_name])
     rows = cur.fetchall()
     for row in rows:
