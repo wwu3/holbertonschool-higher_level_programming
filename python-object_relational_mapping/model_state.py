5b5ba6d6c5ccf97664f21011144a291d3d1ae599
wendy <5703@holbertonstudents.com>
2023-03-18 18:09:26 -0700

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
-
+    """State class"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
