9df8b75586a85d400a07c8889127d66f0516561a
wendy <5703@holbertonstudents.com>
2023-03-18 21:36:26 -0700

    All states via SQLalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- lists all State objects from the database hbtn_0e_6_usa:
+lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
