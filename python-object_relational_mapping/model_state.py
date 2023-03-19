99ec9954a1d8a1575195dedf120190fc0775ea1e
wendy <5703@holbertonstudents.com>
2023-03-18 18:17:28 -0700

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,13 +10,11 @@ Base = declarative_base()
 
 
 class State(Base):
-
     """State class"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
 
     db_uri = ("mysql+mysqldb://root@localhost:3306/hbtn_0e_6_usa")
     engine = create_engine(db_uri)
