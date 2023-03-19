c020fe734670bb6a920faa89ec073d94277d44cf
wendy <5703@holbertonstudents.com>
2023-03-18 20:41:45 -0700

    All states via SQLAIchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,13 +12,17 @@ if __name__ == '__main__':
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
+# connect to the database
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(mysql_name, mysql_password, database_name))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
+# query the database for all State objects and print the results
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}:{}".format(state.id, state.name))
+
+    session.close()
