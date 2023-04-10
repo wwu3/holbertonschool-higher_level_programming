0fa57524fb12015ccc0256fad354a002b59f2cef
Wendy <you@example.com>
2023-04-10 04:26:06 -0700

    argv

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+  console.log('No argument');
+} else if (argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
