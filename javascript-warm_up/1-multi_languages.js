6388096a61e4ecba949b7c4534d49fe1db42de54
Wendy <you@example.com>
2023-04-10 03:06:48 -0700

    print 3 lines

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myvar = 'C is fun';
+const myvar1 = 'Python is cool';
+const myvar2 = 'JavaScript is amazing';
+console.log(myvar);
+console.log(myvar1);
+console.log(myvar2);
