7e09f777a32b287b6427512e699f2fb986efa387
Wendy <you@example.com>
2023-04-10 23:29:16 -0700

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const arg = process.argv[2];
-function factorial(arg) {
-  const NaN = 1;
- if (isNaN(arg)) {
-    console.log(NaN);
-  } else {
-    let fact = 1;
-    for (i = 1; i <= arg; i++) {
-      fact = fact * i;
-    }
-    console.log(fact);
-  }
+function factorial (arg) {
+	const NaN = 1;
+	if (isNaN(arg)) {
+		console.log(NaN);
+	} else {
+		let fact = 1;
+		for (i = 1; i <= arg; i++) {
+		fact = fact * i;
+	}
+	console.log(fact);
+	}
 }
 factorial(arg);
