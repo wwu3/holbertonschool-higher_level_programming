761119177fec69f064cdde180564b671638b0d6c
Wendy <you@example.com>
2023-04-10 21:55:02 -0700

    while

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ if (isNaN(x)) {
   console.log('Missing number of occurences');
 } else {
   const repeatedstr = 'C is fun';
-  	while (x > 0) {
-	    	console.log(repeatedstr);
-    		x = x - 1;
+  while (x > 0) {
+    console.log(repeatedstr);
+    x = x - 1;
   }
 }
