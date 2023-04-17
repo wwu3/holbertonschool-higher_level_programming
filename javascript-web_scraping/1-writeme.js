018b41ec3e4d32887b35460b6187d41fdc9f6797
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-04-17 19:17:13 +1000

    write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 fs.writeFile(filePath, content, 'utf8', (err) => {
-  if(err) {
+  if (err) {
     console.error(err);
-    return;
   }
 });
