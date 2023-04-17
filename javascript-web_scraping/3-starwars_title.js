621c0ff76c66bee710fb217c5bb7ccdbefb5da6e
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-04-17 22:56:13 +1000

    title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const requestURL = 'https://swapi-api.hbtn.io/api/films/:id';
+const request = require('request');
+const id = process.argv[2];
+request(requestURL.replace(':id', id), (err, response, body) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(JSON.parse(body).title);
+});
