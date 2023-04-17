3811ffeb3fd8e3a73b4d06fada2fbc36a66669ee
wendy <5703@holbertonstudents.com>
2023-04-16 23:27:45 -0700

    write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-let filePath = process.argv[2];
 const content = 'Python is cool';
-fs.writeFile(filePath, content, 'utf8', err => {
+fs.writeFile('my_file.txt', content, 'utf8', err => {
   if(err){
-    console.err;
+    console.error(err);
     return;
   }
 });
