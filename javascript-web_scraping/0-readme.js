fda2ffe3a0206e0eaff8849f239417c84059c524
wendy <5703@holbertonstudents.com>
2023-04-16 22:54:20 -0700

    updates utf8

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-let fInput = "C is super fun!"
+let fInput = 'C is super fun!';
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    fInput = data.toString();
-    console.log(fInput);
+  if (err) {
+    console.log(err);
+    return;
+  } 
+  fInput = data.toString();
+  console.log(fInput);
 });
