8c2a75e27fc7dce4110a7cecf083f1ab8746b39a
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-04-20 09:22:05 +1000

    filter

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -7,9 +7,7 @@ request(apiUrl, (err, response, body) => {
     return;
   }
   const films = JSON.parse(body).results;
-  console.log(films);
   const charactersInFilms = films.map((film) => film.characters);
-  console.log(charactersInFilms);
   const countId = charactersInFilms.filter((film) => film.filter(
     character => character.includes('/18/')
   ).length > 0).length;
