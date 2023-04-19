bf0c210604d6766cb355ab6424123da6bc06f949
wendy-holberton <113806658+wendy-holberton@users.noreply.github.com>
2023-04-20 09:01:00 +1000

    indention

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,9 @@ const requestURL = 'https://swapi-api.hbtn.io/api/films/:id';
 const request = require('request');
 const id = process.argv[2];
 request(requestURL.replace(':id', id), (err, response, body) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(JSON.parse(body).title);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(JSON.parse(body).title);
 });
